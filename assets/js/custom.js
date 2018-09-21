0dceb6d5b253bc110a29ad666b437698b98a4e8c
Allen <allen@webriq.services>
2018-09-21 10:32:49 +0800

    first commit new layout

---
+++ assets/js/custom.js
@@ -0,0 +1,52 @@
+$("#menu").sticky({topSpacing:0});
+(function() {
+    var menuEl = document.getElementById('ml-menu'),
+        mlmenu = new MLMenu(menuEl, {
+            // breadcrumbsCtrl : true, // show breadcrumbs
+            // initialBreadcrumb : 'all', // initial breadcrumb text
+            backCtrl : false, // show back button
+            // itemsDelayInterval : 60, // delay between each menu item sliding animation
+            onItemClick: false, // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
+            direction: 'l2r'
+        });
+
+    // mobile menu toggle
+    var openMenuCtrl = document.querySelector('.action--open'),
+        closeMenuCtrl = document.querySelector('.action--close');
+
+    openMenuCtrl.addEventListener('click', function(){
+        $('.menu').toggleClass('menu--open');
+        $('body').toggleClass('menu--opened')
+    });
+    closeMenuCtrl.addEventListener('click', closeMenu);
+
+    function openMenu() {
+        classie.add(menuEl, 'menu--open');
+        closeMenuCtrl.focus();
+    }
+
+    function closeMenu() {
+        classie.remove(menuEl, 'menu--open');
+        $('body').removeClass('menu--opened')
+        openMenuCtrl.focus();
+    }
+
+})();
+var $menu = $('.menu');
+
+$(document).mouseup(function (e) {
+   if (!$menu.is(e.target) // if the target of the click isn't the container...
+   && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+   {
+	 $('body').removeClass('menu--opened');
+     $menu.removeClass('menu--open');
+  }
+ });
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=1037601643039892&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
