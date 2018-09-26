ec9bc2308d66811fdeeb7219589fc06ac56db72b
Allen <allen@webriq.services>
2018-09-26 17:22:21 +0800

    from spike to gatsby

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/allen/Projects/htdocs/webriqpage-anzacday/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/allen/Projects/htdocs/webriqpage-anzacday/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
