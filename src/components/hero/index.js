b395d51ebe262f9d34e3598a9649f1432fc0722d
Allen <allen@webriq.services>
2018-10-17 11:36:00 +0800

    hero index

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {Container, Row, Col, Media} from 'react-bootstrap'
 // import Slide_1600w from '../../images/slide4-1600w.jpg'
+import { Match } from "@reach/router"
 import './index.css'
 
 
-export default class Hero extends Component {
-  render () {
-    if (window.location.pathname === '/'){
-      return (
+// export default class Hero extends Component {
+//   render () {
+//     if (window.location.pathname === '/'){
+//       return (
+//         <div className='hero'>
+//           <Media>
+//             <img className='w-100 mb-0' src='https://res.cloudinary.com/db6xmqefe/image/upload/c_scale,w_414/v1538371271/slide.jpg' 
+//             srcSet='https://res.cloudinary.com/db6xmqefe/image/upload/c_scale,w_414/v1538371271/slide.jpg 414w,
+//                       https://res.cloudinary.com/db6xmqefe/image/upload/c_scale,w_568/v1538371271/slide.jpg 568w,
+//                       https://res.cloudinary.com/db6xmqefe/image/upload/c_scale,w_667/v1538371271/slide.jpg 667w,
+//                       https://res.cloudinary.com/db6xmqefe/image/upload/c_scale,w_767/v1538371271/slide.jpg 767w,
+//                       https://res.cloudinary.com/db6xmqefe/image/upload/c_scale,w_823/v1538371271/slide.jpg 823w,
+//                       https://res.cloudinary.com/db6xmqefe/image/upload/c_scale,w_1366/v1538371271/slide.jpg 1366w,
+//                       https://res.cloudinary.com/db6xmqefe/image/upload/c_scale,w_1600/v1538371271/slide.jpg 1600w' alt=''/>
+//           </Media>
+//         </div>
+//       )
+//     }
+//     else {
+//       return (
+//         <div className='hero-main'>
+//           <Container>
+//             <Row>
+//               <Col sm={8} md={7}>
+//                 <h1 className='titlepage'>{this.props.children}</h1>
+//               </Col>
+//             </Row>
+//           </Container>
+//         </div>
+//       )
+//     }
+//   }
+// }
+const Hero = () => (
+  <Match path="/">
+    {props =>
+      props.match ? (
         <div className='hero'>
           <Media>
             <img className='w-100 mb-0' src='https://res.cloudinary.com/db6xmqefe/image/upload/c_scale,w_414/v1538371271/slide.jpg' 
@@ -20,26 +54,23 @@ export default class Hero extends Component {
                       https://res.cloudinary.com/db6xmqefe/image/upload/c_scale,w_1600/v1538371271/slide.jpg 1600w' alt=''/>
           </Media>
         </div>
-      )
-    }
-    else {
-      return (
+      ) : (
         <div className='hero-main'>
           <Container>
             <Row>
               <Col sm={8} md={7}>
-                <h1 className='titlepage'>{this.props.children}</h1>
               </Col>
             </Row>
           </Container>
         </div>
       )
     }
-  }
-}
+  </Match>
 
+)
+export default Hero
 
-// const Hero = ({props, children}) => {
+// const Hero = () => {
   
 //   if (window.location.pathname === '/'){
 //     return (
