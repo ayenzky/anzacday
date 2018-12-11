6aa12d96b0554fa28b2ea39f526a0a19d5a329f6
Allen <allen@webriq.services>
2018-12-11 13:54:08 +0800

    add pages.css

--- src/pages/anzac-news.js
+++ src/pages/anzac-news.js
@@ -4,6 +4,7 @@ import Moment from 'react-moment'
 import Layout from '../components/layout'
 import {Row, Col} from 'react-bootstrap'
 import './anzac-news.css'
+import './pages.css'
 
 
 const IndexPage = ({ data }) => (  
