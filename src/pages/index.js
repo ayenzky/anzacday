d28d074b2f23af1d59bc69a9832c75804257d568
Allen <allen@webriq.services>
2019-08-01 10:44:43 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -126,7 +126,6 @@ const IndexPage = ({data}) => (
                     <p><strong>Unit 2<br/> 14 Boothby Street<br/> Kedron 4031</strong></p>
                     <ul className="list-unstyled mt-3 ml-0">
                     <li>Tel.: <a href="tel:0732637118" title="0732637118">07 3263 7118</a></li>
-                    <li>Fax: <a href="tel:0731750608" title="0731750608">07 3175 0608</a></li>
                     <li>Email:&nbsp;<a href="mailto:office.adcc@anzacday.org.au" title="office.adcc@anzacday.org.au">office.adcc@anzacday.org.au</a></li>
                   </ul>
                   </div>
